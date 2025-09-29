4472cfa0a74323cfd05a3216339be9f40cce9a91
Enzo Claeyssen <enzo.claeyssen@gmail.com>
2025-09-29 16:33:50 +0200

    feat: Finished following advice of pylint + removed pycache of tests

--- tests/test_ding_dong_ditch.py
+++ tests/test_ding_dong_ditch.py
@@ -6,22 +6,37 @@ Created on Fri Sep 26 11:24:13 2025
 @author: enzo
 """
 
-import pytest
 from kattis.ding_dong_ditch import ding_dong_ditch
 
 
 
 def test_sample1() :
-    N, Q = 4, 2
+    """
+    Simple test
+
+    Returns
+    -------
+    None.
+
+    """
+    n, q = 4, 2
     angers = [3, 2, 1, 4]
     expectations = [3, 1]
     expected = [6, 1]
-    assert ding_dong_ditch(N, Q, angers, expectations) == expected
+    assert ding_dong_ditch(n, q, angers, expectations) == expected
 
 
 def test_sample2() :
-    N, Q = 5, 1
+    """
+    Second test
+
+    Returns
+    -------
+    None.
+
+    """
+    n, q = 5, 1
     angers = [12, 2, 14, 6, 9]
     expectations = [3]
     expected = [17]
-    assert ding_dong_ditch(N, Q, angers, expectations) == expected
+    assert ding_dong_ditch(n, q, angers, expectations) == expected
